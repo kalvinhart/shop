d3b4fc21dc7f0de2e06ee48f3bf5de9cb15e66b8
Kalvin <kalvinhart@hotmail.co.uk>
2022-02-23 21:56:21 +0000

    Added async error handling to controller.

--- api/src/controllers/productController.js
+++ api/src/controllers/productController.js
@@ -6,50 +6,34 @@ const getAllProducts = catchAsync(async (req, res, next) => {
   res.json(products);
 });
 
-const getProduct = async (req, res, next) => {
+const getProduct = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-  try {
-    const product = await Product.findById(id);
-    res.json(product);
-  } catch (error) {
-    return next(new NoResultsError(id));
-  }
-};
+  const product = await Product.findById(id);
+  res.json(product);
+});
 
-const addNewProduct = async (req, res, next) => {
+const addNewProduct = catchAsync(async (req, res, next) => {
   const product = req.body;
   const newProduct = new Product(product);
-  try {
-    const saved = await newProduct.save();
-    res.json(saved);
-  } catch (error) {
-    return next(new CustomError(error.message, 500));
-  }
-};
+  const saved = await newProduct.save();
+  res.json(saved);
+});
 
-const updateProduct = async (req, res, next) => {
+const updateProduct = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const updatedProduct = req.body;
-  try {
-    const updated = await Product.findByIdAndUpdate(id, updatedProduct, {
-      runValidators: true,
-      new: true,
-    });
-    res.json(updated);
-  } catch (error) {
-    return next(new CustomError(error.message, 500));
-  }
-};
+  const updated = await Product.findByIdAndUpdate(id, updatedProduct, {
+    runValidators: true,
+    new: true,
+  });
+  res.json(updated);
+});
 
-const deleteProduct = async (req, res, next) => {
+const deleteProduct = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-  try {
-    const deleted = await Product.deleteOne({ _id: id });
-    res.json(deleted);
-  } catch (error) {
-    return next(new CustomError(error.message, 500));
-  }
-};
+  const deleted = await Product.deleteOne({ _id: id });
+  res.json(deleted);
+});
 
 module.exports = {
   getAllProducts,
