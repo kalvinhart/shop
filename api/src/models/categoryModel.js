97cef78aa2485098df625ac74f34c788d04590f7
Kalvin <kalvinhart@hotmail.co.uk>
2022-02-23 17:46:24 +0000

    Added Category model Schema.

---
+++ api/src/models/categoryModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: String,
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Category", categorySchema);
