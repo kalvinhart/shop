516772cb0e52e11f6bf3ff186fa20953eecb6181
Kalvin <kalvinhart@hotmail.co.uk>
2022-02-23 17:49:14 +0000

    Amended Category model Schema.

--- api/src/models/categoryModel.js
+++ api/src/models/categoryModel.js
@@ -4,12 +4,6 @@ const { Schema } = mongoose;
 
 const categorySchema = new Schema({
   name: String,
-  products: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
-    },
-  ],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
