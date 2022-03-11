0a0843bffc013bc01277b52490fcaac4310cb168
Kalvin <kalvinhart@hotmail.co.uk>
2022-03-11 16:07:59 +0000

    Removed redundant username variables.

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -2,8 +2,8 @@ const User = require("../models/userModel");
 const { catchAsync } = require("../middleware/errors");
 
 const registerUser = catchAsync(async (req, res, next) => {
-  const { email, username, password } = req.body;
-  const user = new User({ email, username });
+  const { email, password } = req.body;
+  const user = new User({ email });
   const newUser = await User.register(user, password);
   res.status(201).json(newUser);
 });
