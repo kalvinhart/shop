3849d9fe843128acf5e39582d991cecc9e0c2ad5
Kalvin <kalvinhart@hotmail.co.uk>
2022-03-23 13:57:22 +0000

    Updated header UI styles.

--- client/src/components/shared/Header/Header.js
+++ client/src/components/shared/Header/Header.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAlt, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faUserAlt, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 import {
   StyledCartCount,
   StyledCartIconWrapper,
   StyledHeader,
   StyledHeaderWrapper,
+  StyledSearchButton,
+  StyledSearchForm,
   StyledUserInfoWrapper,
 } from "./Header.styles";
-import Container from "../Container/Container";
 import { SpanLogo } from "../../../styles/fontStyles";
 import { StyledInput } from "../../../styles/formStyles";
 import { Button } from "../../../styles/buttonStyles";
@@ -17,46 +18,49 @@ import { Button } from "../../../styles/buttonStyles";
 const Header = ({ loading, user, logOut, cart }) => {
   return (
     <StyledHeader>
-      <Container>
-        <StyledHeaderWrapper>
-          <SpanLogo as={Link} to="/">
-            My eShop
-          </SpanLogo>
+      <StyledHeaderWrapper>
+        <SpanLogo as={Link} to="/">
+          My eShop
+        </SpanLogo>
 
+        <StyledSearchForm>
           <StyledInput type="text" name="search" id="search" placeholder="Search" />
+          <StyledSearchButton>
+            <FontAwesomeIcon icon={faSearch} size="lg" />
+          </StyledSearchButton>
+        </StyledSearchForm>
 
-          <StyledUserInfoWrapper>
-            {loading ? null : user ? (
-              <>
-                <Link to="/profile">
-                  <FontAwesomeIcon className="icon" icon={faUserAlt} size="lg" />
-                </Link>
-                <Button $primary onClick={logOut}>
-                  Log out
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button as={Link} to="/login" $primary>
-                  Login
-                </Button>
-                <Button as={Link} to="/register" $secondary>
-                  Register
-                </Button>
-              </>
-            )}
+        <StyledUserInfoWrapper>
+          {loading ? null : user ? (
+            <>
+              <Link to="/profile">
+                <FontAwesomeIcon className="icon" icon={faUserAlt} size="lg" />
+              </Link>
+              <Button $primary onClick={logOut}>
+                Log out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button as={Link} to="/login" $primary>
+                Login
+              </Button>
+              <Button as={Link} to="/register" $secondary>
+                Register
+              </Button>
+            </>
+          )}
 
-            <Link to="/cart">
-              <StyledCartIconWrapper>
-                {cart && cart.cartCount > 0 && (
-                  <StyledCartCount>{cart.cartCount}</StyledCartCount>
-                )}
-                <FontAwesomeIcon className="icon" icon={faCartShopping} size="lg" />
-              </StyledCartIconWrapper>
-            </Link>
-          </StyledUserInfoWrapper>
-        </StyledHeaderWrapper>
-      </Container>
+          <Link to="/cart">
+            <StyledCartIconWrapper>
+              {cart && cart.cartCount > 0 && (
+                <StyledCartCount>{cart.cartCount}</StyledCartCount>
+              )}
+              <FontAwesomeIcon className="icon" icon={faCartShopping} size="lg" />
+            </StyledCartIconWrapper>
+          </Link>
+        </StyledUserInfoWrapper>
+      </StyledHeaderWrapper>
     </StyledHeader>
   );
 };
--- client/src/components/shared/Header/Header.styles.js
+++ client/src/components/shared/Header/Header.styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { StyledInput } from "../../../styles/formStyles";
 
 export const StyledHeaderWrapper = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
 `;
@@ -11,12 +13,42 @@ export const StyledHeader = styled.header`
   left: 0;
   height: var(--header-height);
   width: 100%;
-  padding: 10px 20px;
+  padding: 10px 60px;
   display: flex;
-  justify-content: center;
   align-items: center;
   background-color: white;
   box-shadow: var(--box-shadow);
+`;
+
+export const StyledSearchForm = styled.form`
+  position: relative;
+  width: 400px;
+  height: 40px;
+
+  & ${StyledInput} {
+    width: 100%;
+    height: 100%;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+
+export const StyledSearchButton = styled.button`
+  position: absolute;
+  right: -40px;
+  top: 0;
+  width: 40px;
+  height: 40px;
+  border: 2px solid transparent;
+  background-color: #eee;
+  border-top-right-radius: var(--border-radius);
+  border-bottom-right-radius: var(--border-radius);
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: #ddd;
+  }
 `;
 
 export const StyledUserInfoWrapper = styled.div`
