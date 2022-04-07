a6f78d2661082bc2753871653662c18418481e4b
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-07 15:12:26 +0100

    Added media queries.

--- client/src/components/HomePage/SideBar/SideBar.styles.js
+++ client/src/components/HomePage/SideBar/SideBar.styles.js
@@ -1,14 +1,21 @@
 import styled from "styled-components";
+import mediaSizes from "../../../styles/media";
 
 export const StyledSideBarBackground = styled.div`
-  width: 240px;
+  width: 100%;
   padding: 20px;
-  margin-right: 20px;
+  margin: 0 0 20px 0;
   display: flex;
   flex-direction: column;
   background-color: white;
   border: var(--borders);
   box-shadow: var(--box-shadow-small);
+  overflow-x: auto;
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    width: 240px;
+    margin: 0 20px 0 0;
+  }
 `;
 
 export const StyledNav = styled.nav``;
@@ -18,11 +25,15 @@ export const StyledNavUL = styled.ul`
   margin-block-end: 0;
   padding-inline-start: 10px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   list-style: none;
 
   & > *:not(:last-child) {
     margin-bottom: 20px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    flex-direction: column;
   }
 `;
 
