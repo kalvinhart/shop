9af71d8ab9a05d236d4fb1c899bb370decfbfd1e
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-07 22:49:28 +0100

    Added media queries.

--- client/src/components/shared/PageWrapper/PageWrapper.styles.js
+++ client/src/components/shared/PageWrapper/PageWrapper.styles.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import mediaSizes from "../../../styles/media";
 
 export const StyledPageWrapper = styled.div`
   width: 100%;
-  margin-top: calc(var(--header-height) + 60px);
+  margin-top: calc(var(--header-height) + 90px);
   margin-bottom: 60px;
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    margin-top: calc(var(--header-height) + 60px);
+  }
 `;
