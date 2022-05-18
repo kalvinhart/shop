d24c586e9713cfdc33883f5783fc78072ae96f29
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-18 15:39:44 +0100

    Refactored to use custom hook.

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,49 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router";
-import { logInUser } from "../../actions/authActions";
+import { useLoginPage } from "../../hooks/useLoginPage/useLoginPage";
 
 import AuthForm from "../shared/AuthForm/AuthForm";
 
 const LoginPage = () => {
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const logIn = useSelector((state) => state.auth);
-  const { loading, user, error } = logIn;
-
-  useEffect(() => {
-    if (user) navigate(location.state?.from ? location.state.from : "/");
-  }, [user]);
-
-  const inputConfig = {
-    email: {
-      value: "",
-      options: {
-        required: true,
-        min: 6,
-      },
-    },
-    password: {
-      value: "",
-      options: {
-        required: true,
-        min: 8,
-      },
-    },
-  };
-
-  const formSubmit = (formValues) => {
-    const { email, password } = formValues;
-
-    dispatch(
-      logInUser({
-        email: email.value,
-        password: password.value,
-      })
-    );
-  };
+  const { loading, error, inputConfig, formSubmit } = useLoginPage();
 
   return (
     <AuthForm
