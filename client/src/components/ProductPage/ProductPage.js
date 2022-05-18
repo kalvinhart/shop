d12e0a351bc54f15443cd844b12df134e7e3e39d
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-18 16:39:10 +0100

    Refactored to use custom hook.

--- client/src/components/ProductPage/ProductPage.js
+++ client/src/components/ProductPage/ProductPage.js
@@ -1,34 +1,10 @@
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-
-import { loadProductDetails } from "../../actions/productActions";
-import { addToCart } from "../../actions/cartActions";
+import { useProductPage } from "../../hooks/useProductPage/useProductPage";
 
 import ProductDetails from "./ProductDetails/ProductDetails";
 import Spinner from "../shared/Spinner/Spinner";
 
 const ProductPage = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const params = useParams();
-
-  const productDetails = useSelector((state) => state.productDetails);
-  const { loading, error, product } = productDetails;
-
-  const { id } = params;
-
-  useEffect(() => {
-    dispatch(loadProductDetails(id));
-  }, []);
-
-  useEffect(() => {
-    if (error) navigate("/");
-  }, [error]);
-
-  const handleAddToCart = (item) => {
-    dispatch(addToCart(item));
-  };
+  const { loading, product, error, handleAddToCart } = useProductPage();
 
   return (
     <>
