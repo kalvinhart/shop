065731f4f1949e1ca65eb9dc64ec8a6885c224f9
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-18 14:30:11 +0100

    Refactored to use custom hook.

--- client/src/components/HomePage/Filters/Filters.js
+++ client/src/components/HomePage/Filters/Filters.js
@@ -1,8 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-
-import { updateSearchOptions } from "../../../actions/productActions";
+import { useFilters } from "../../../hooks/useFilters/useFilters";
 
 import {
   StyledFiltersBackground,
@@ -11,73 +7,16 @@ import {
 } from "./Filters.styles";
 import { H3, SpanBold } from "../../../styles/fontStyles";
 import { StyledSelect } from "../../../styles/formStyles";
-import { Button } from "../../../styles/buttonStyles";
 
 const Filters = () => {
-  const dispatch = useDispatch();
-  const { count, searchOptions } = useSelector((state) => state.products);
-
-  const hasOptions = Object.keys(searchOptions).length > 0;
-
-  const handleRemoveOptions = (option) => {
-    dispatch(updateSearchOptions(option, ""));
-  };
-
-  let optionsTags = [];
-
-  if (hasOptions) {
-    Object.entries(searchOptions).forEach((item) => {
-      const [optionName, optionValue] = item;
-
-      if (optionName === "sortBy") return;
-
-      const formattedOptionName = `${optionName
-        .slice(0, 1)
-        .toUpperCase()}${optionName.slice(1)}`;
-
-      optionsTags.push(
-        <Button
-          type="filter"
-          key={optionValue}
-          onClick={() => handleRemoveOptions(optionName)}
-        >
-          {`${formattedOptionName}: ${optionValue}`}
-          <FontAwesomeIcon icon={faTimes} />
-        </Button>
-      );
-    });
-  }
-
-  const sortOptions = [
-    {
-      name: "-amountSold",
-      text: "Most Popular",
-    },
-    {
-      name: "createdAt",
-      text: "Most Recent",
-    },
-    {
-      name: "price",
-      text: "Price - Low to High",
-    },
-    {
-      name: "-price",
-      text: "Price - High to Low",
-    },
-    {
-      name: "name",
-      text: "Name - Ascending",
-    },
-    {
-      name: "-name",
-      text: "Name - Descending",
-    },
-  ];
-
-  const handleSelectChange = (e) => {
-    dispatch(updateSearchOptions("sortBy", e.target.value));
-  };
+  const {
+    count,
+    sortOptions,
+    hasOptions,
+    optionsTags,
+    searchOptions,
+    handleSelectChange,
+  } = useFilters();
 
   return (
     <StyledFiltersBackground>
