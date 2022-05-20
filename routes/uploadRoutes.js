fa22e9eb1f06aa3bb28639fb1f3969c5bb314677
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-20 20:42:02 +0100

    Refactored image upload to utilise AWS S3.

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { uploadFile } = require("../controllers/uploadController");
+const { generateUploadUrl } = require("../middleware/s3");
 
+router.post("/s3url", generateUploadUrl);
 router.post("/", uploadFile);
 
 module.exports = router;
