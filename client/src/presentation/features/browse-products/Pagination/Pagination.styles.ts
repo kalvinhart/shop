3c29c248eca6f1a878eeb64b4dfd42883fb00db2
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-01 14:29:42 +0100

    style: adjust responsive style.

--- client/src/presentation/features/browse-products/Pagination/Pagination.styles.ts
+++ client/src/presentation/features/browse-products/Pagination/Pagination.styles.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
+import { mediaSizes } from "../../../common/styles";
 
 export const PaginationWrapper = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
+  @media screen and (min-width: ${mediaSizes.med}) {
+    flex-direction: row;
+  }
 `;
 
 export const PaginationResultsWrapper = styled.div``;
