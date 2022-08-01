321ebb148623ce2e6e177f86b8606357dfb3b7a8
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-01 13:49:18 +0100

    refactor: decoupled controller from framework

--- features/categories/categoryController.js
+++ features/categories/categoryController.js
@@ -1,32 +1,24 @@
 const Category = require("./categoryModel");
 const { catchAsync } = require("../../middleware/errors");
 
-const getAllCategories = catchAsync(async (req, res, next) => {
-  const categories = await Category.find({});
-  res.status(200).json(categories);
-});
+const getAllCategories = async () => {
+  return await Category.find({});
+};
 
-const createNewCategory = catchAsync(async (req, res, next) => {
-  const { name } = req.body;
+const createNewCategory = async (name) => {
   const newCategory = new Category({ name });
-  const saved = await newCategory.save();
-  res.status(201).json(saved);
-});
+  return await newCategory.save();
+};
 
-const updateCategory = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const updatedCategory = await Category.findByIdAndUpdate(id, name, {
+const updateCategory = async (id, name) => {
+  return await Category.findByIdAndUpdate(id, name, {
     runValidators: true,
     new: true,
   });
-  res.status(201).json(updatedCategory);
-});
+};
 
 const deleteCategory = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const deleted = await Category.deleteOne({ _id: id });
-  res.status(200).json(deleted);
+  return await Category.deleteOne({ _id: id });
 });
 
 module.exports = {
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -7,10 +7,46 @@ const {
   updateCategory,
   deleteCategory,
 } = require("../features/categories/categoryController");
+const { catchAsync } = require("../middleware/errors");
 
-router.get("/", getAllCategories);
-router.post("/", createNewCategory);
-router.patch("/:id", updateCategory);
-router.delete("/:id", deleteCategory);
+// Get All Categories - GET - api/categories/
+router.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const response = await getAllCategories();
+    res.status(200).json(response);
+  })
+);
+
+// Create New Category - POST - api/categories/
+router.post(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const { name } = req.body;
+    const response = createNewCategory(name);
+    res.status(201).json(response);
+  })
+);
+
+// Update Category - PATCH - api/categories/:id
+router.patch(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const response = await updateCategory(id, name);
+    res.status(201).json(response);
+  })
+);
+
+// Delete Category - DELETE - api/categories/:id
+router.delete(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const response = await deleteCategory(id);
+    res.status(200).json(response);
+  })
+);
 
 module.exports = router;
