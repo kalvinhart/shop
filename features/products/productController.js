c970c7f45b35c21e532dea4269f6638e25b4ba7d
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-01 13:41:50 +0100

    refactor: decoupled controller from framework

--- features/products/productController.js
+++ features/products/productController.js
@@ -1,8 +1,7 @@
 const Product = require("./productModel");
-const { catchAsync } = require("../../middleware/errors");
 const { buildQueryFromSearchOptions } = require("./utils/buildQueryFromSearchOptions");
 
-const getAllProducts = catchAsync(async (req, res, next) => {
+const getAllProducts = async (requestQuery) => {
   let {
     name,
     category,
@@ -12,7 +11,7 @@ const getAllProducts = catchAsync(async (req, res, next) => {
     sort = "-amountSold",
     page = 1,
     pagesize: pageSize = 10,
-  } = req.query;
+  } = requestQuery;
 
   page = Number(page);
   pageSize = Number(pageSize);
@@ -48,70 +47,42 @@ const getAllProducts = catchAsync(async (req, res, next) => {
       totalPages,
     },
   };
-  res.json(response);
-});
+  return response;
+};
 
-const getProduct = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.json(product);
-});
+const getProduct = async (id) => {
+  return await Product.findById(id);
+};
 
-const getProductsByCategory = catchAsync(async (req, res, next) => {
-  const { categoryName } = req.params;
-  const products = await Product.find({
+const getProductsByCategory = async (categoryName) => {
+  return await Product.find({
     categories: { $in: [categoryName] },
-  });
-  if (products.length === 0) {
-    res.status(404).json({ msg: "No products were found in this category." });
-  } else {
-    res.json(products);
-  }
-});
-
-const addNewProduct = catchAsync(async (req, res, next) => {
-  const product = req.body;
-  const newProduct = new Product(product);
-  const saved = await newProduct.save();
-  res.json(saved);
-});
-
-const updateProduct = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const updatedProduct = req.body;
-  const updated = await Product.findByIdAndUpdate(id, updatedProduct, {
-    runValidators: true,
-    new: true,
-  });
-  res.json(updated);
-});
-
-const deleteProduct = (req, res, next) => {
-  const { id } = req.params;
-  Product.findOneAndDelete({ _id: id }, (err, doc) => {
-    if (err) {
-      return next(new Error(err));
-    } else {
-      res.json(doc._id);
-    }
   });
 };
 
-const deleteManyProducts = catchAsync(async (req, res, next) => {
-  const { ids } = req.body;
-  const result = await Product.deleteMany({
+const addNewProduct = async (product) => {
+  const newProduct = new Product(product);
+  return await newProduct.save();
+};
+
+const updateProduct = async (id, updatedProduct) => {
+  return await Product.findByIdAndUpdate(id, updatedProduct, {
+    runValidators: true,
+    new: true,
+  });
+};
+
+const deleteProduct = (id) => {
+  return Product.findOneAndDelete({ _id: id });
+};
+
+const deleteManyProducts = async (ids) => {
+  return await Product.deleteMany({
     _id: { $in: ids },
   });
+};
 
-  if (result.deletedCount === 0) {
-    return next(new Error(`No products were found matching the ID's provided: ${ids}`));
-  }
-
-  res.json(result);
-});
-
-const getAllFilters = catchAsync(async (req, res, next) => {
-  console.log("Made it to function.");
+const getAllFilters = async () => {
   const allBrands = await Product.aggregate([
     {
       $group: {
@@ -148,8 +119,8 @@ const getAllFilters = catchAsync(async (req, res, next) => {
     },
   ]);
 
-  res.json({ allBrands, allColors, allSizes });
-});
+  return { allBrands, allColors, allSizes };
+};
 
 module.exports = {
   getAllProducts,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { catchAsync } = require("../middleware/errors");
+
 const {
   getAllProducts,
   getProduct,
@@ -12,13 +14,88 @@ const {
   getAllFilters,
 } = require("../features/products/productController");
 
-router.get("/", getAllProducts);
-router.get("/filters", getAllFilters);
-router.get("/:id", getProduct);
-router.get("/category/:categoryName", getProductsByCategory);
-router.post("/new", addNewProduct);
-router.patch("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
-router.patch("/", deleteManyProducts);
+router.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const { query } = req;
+
+    const response = await getAllProducts(query);
+
+    res.json(response);
+  })
+);
+
+router.get(
+  "/filters",
+  catchAsync(async (req, res, next) => {
+    const response = await getAllFilters();
+    res.json(response);
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const response = await getProduct(id);
+    res.json(response);
+  })
+);
+
+router.get(
+  "/category/:categoryName",
+  catchAsync(async (req, res, next) => {
+    const { categoryName } = req.params;
+    const response = await getProductsByCategory(categoryName);
+
+    if (response.length === 0) {
+      res.status(404).json({ msg: "No products were found in this category." });
+    } else {
+      res.json(response);
+    }
+  })
+);
+
+router.post(
+  "/new",
+  catchAsync(async (req, res, next) => {
+    const product = req.body;
+    const response = await addNewProduct(product);
+    res.json(response);
+  })
+);
+
+router.patch(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const updatedProduct = req.body;
+    const response = await updateProduct(id, updatedProduct);
+    res.json(response);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const response = deleteProduct(id);
+    res.json(response);
+  })
+);
+
+router.patch(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const { ids } = req.body;
+    const response = await deleteManyProducts(ids);
+
+    if (response.deletedCount === 0) {
+      return next(new Error(`No products were found matching the ID's provided: ${ids}`));
+    }
+
+    res.json(response);
+  })
+);
 
 module.exports = router;
