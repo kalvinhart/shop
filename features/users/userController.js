dd07f8dec62301097aa8bbbb616e192e01a27ac3
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-02 20:39:35 +0100

    refactor: moved logic to separate service file.

--- features/users/userController.js
+++ features/users/userController.js
@@ -1,78 +1,15 @@
-const jwt = require("jsonwebtoken");
-const { hashSync, compareSync } = require("bcrypt");
-
-const User = require("./userModel");
-const ROLES = require("../../config/userRoles");
+const userService = require("../../services/userService");
 
 const registerUser = async (email, password) => {
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    throw new Error("An account already exists with this email address.");
-  }
-
-  const hashPassword = await hashSync(password, 12);
-
-  const user = new User({ email, password: hashPassword, role: ROLES.User });
-  const newUser = await user.save();
-
-  const userCredentials = {
-    email: newUser.email,
-    id: newUser._id,
-    role: ROLES.User,
-  };
-
-  const token = jwt.sign(userCredentials, process.env.JWT_SECRET, { expiresIn: "7d" });
-
-  const payload = {
-    ...userCredentials,
-    token: `Bearer ${token}`,
-    wishlist: user.wishlist,
-  };
-
-  return payload;
+  return await userService.register(email, password);
 };
 
 const logInUser = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("Incorrect username/password.");
-  }
-
-  const passwordMatch = await compareSync(password, user.password);
-  if (!passwordMatch) {
-    throw new Error("Incorrect username/password.");
-  }
-
-  const userCredentials = {
-    email: user.email,
-    id: user._id,
-    role: user.role,
-  };
-
-  const token = jwt.sign(userCredentials, process.env.JWT_SECRET, { expiresIn: "7d" });
-
-  const payload = {
-    ...userCredentials,
-    token: `Bearer ${token}`,
-    wishlist: user.wishlist,
-  };
-
-  return payload;
+  return await userService.signIn(email, password);
 };
 
 const getUserInfo = async (id, token) => {
-  const user = await User.findOne({ _id: id });
-  if (!user) throw new Error("User does not exist.");
-
-  const payload = {
-    id: user._id,
-    email: user.email,
-    role: user.role,
-    token,
-    wishlist: user.wishlist,
-  };
-
-  return payload;
+  return await userService.getUser(id, token);
 };
 
 module.exports = {
---
+++ services/userService.js
@@ -0,0 +1,79 @@
+const User = require("../features/users/userModel");
+
+const jwt = require("jsonwebtoken");
+const { hashSync, compareSync } = require("bcrypt");
+
+const ROLES = require("../config/userRoles");
+
+const userService = {
+  async register(email, password) {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      throw new Error("An account already exists with this email address.");
+    }
+
+    const hashPassword = await hashSync(password, 12);
+
+    const user = new User({ email, password: hashPassword, role: ROLES.User });
+    const newUser = await user.save();
+
+    const userCredentials = {
+      email: newUser.email,
+      id: newUser._id,
+      role: ROLES.User,
+    };
+
+    const token = jwt.sign(userCredentials, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    const payload = {
+      ...userCredentials,
+      token: `Bearer ${token}`,
+      wishlist: user.wishlist,
+    };
+
+    return payload;
+  },
+  async signIn(email, password) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Incorrect username/password.");
+    }
+
+    const passwordMatch = await compareSync(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Incorrect username/password.");
+    }
+
+    const userCredentials = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(userCredentials, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    const payload = {
+      ...userCredentials,
+      token: `Bearer ${token}`,
+      wishlist: user.wishlist,
+    };
+
+    return payload;
+  },
+  async getUser(id, token) {
+    const user = await User.findOne({ _id: id });
+    if (!user) throw new Error("User does not exist.");
+
+    const payload = {
+      id: user._id,
+      email: user.email,
+      role: user.role,
+      token,
+      wishlist: user.wishlist,
+    };
+
+    return payload;
+  },
+};
+
+module.exports = userService;
