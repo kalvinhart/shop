acaebbce0d9ee2c0670af71b1a79b244834ea4b7
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-02 20:49:40 +0100

    refactor: moved logic to separate service file.

--- features/payments/paymentController.js
+++ features/payments/paymentController.js
@@ -1,11 +1,5 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-const Order = require("../orders/orderModel");
 const paymentService = require("../../services/paymentService");
 const calculateOrderAmount = require("./utils/calculateOrderAmount");
-
-const endpointSecret =
-  "whsec_be9c315a348e616c83d051c5c5987587210c7390b04fb9ca27bfda9c663519a7";
 
 const createIntent = async (items, user) => {
   const total = await calculateOrderAmount(items);
@@ -21,49 +15,7 @@ const createIntent = async (items, user) => {
 };
 
 const handleWebhook = async (event, signature) => {
-  console.log("Reached Webhook...");
-  if (endpointSecret) {
-    constructedEvent = stripe.webhooks.constructEvent(event, signature, endpointSecret);
-  }
-
-  if (constructedEvent.type === "payment_intent.succeeded") {
-    const paymentIntent = constructedEvent.data.object;
-    const {
-      id,
-      amount,
-      metadata,
-      shipping: { address, name },
-    } = paymentIntent;
-    console.log(paymentIntent);
-
-    const metadataItems = JSON.parse(metadata.items);
-    const orderItems = [];
-
-    Object.keys(metadataItems).forEach((item) => {
-      const singleItem = {
-        id: item,
-        qty: metadataItems[item].qty,
-        total: metadataItems[item].total,
-      };
-      orderItems.push(singleItem);
-    });
-
-    const order = {
-      piKey: id,
-      userId: metadata.userId,
-      amount,
-      items: orderItems,
-      shipping: {
-        address,
-        name,
-      },
-    };
-
-    console.log("order: ", order);
-    const newOrder = new Order(order);
-    await newOrder.save();
-    console.log("Order saved!");
-  }
+  await paymentService.handleWebhook(event, signature);
 };
 
 module.exports = {
--- services/paymentService.js
+++ services/paymentService.js
@@ -1,5 +1,10 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const endpointSecret =
+  "whsec_be9c315a348e616c83d051c5c5987587210c7390b04fb9ca27bfda9c663519a7";
+
+const Order = require("../features/orders/orderModel");
+
 const paymentService = {
   async createIntent({ items, user, total }) {
     console.log(items, total);
@@ -12,6 +17,51 @@ const paymentService = {
     });
 
     return paymentIntent;
+  },
+  async handleWebhook(event, signature) {
+    console.log("Reached Webhook...");
+    if (endpointSecret) {
+      constructedEvent = stripe.webhooks.constructEvent(event, signature, endpointSecret);
+    }
+
+    if (constructedEvent.type === "payment_intent.succeeded") {
+      const paymentIntent = constructedEvent.data.object;
+      const {
+        id,
+        amount,
+        metadata,
+        shipping: { address, name },
+      } = paymentIntent;
+      console.log(paymentIntent);
+
+      const metadataItems = JSON.parse(metadata.items);
+      const orderItems = [];
+
+      Object.keys(metadataItems).forEach((item) => {
+        const singleItem = {
+          id: item,
+          qty: metadataItems[item].qty,
+          total: metadataItems[item].total,
+        };
+        orderItems.push(singleItem);
+      });
+
+      const order = {
+        piKey: id,
+        userId: metadata.userId,
+        amount,
+        items: orderItems,
+        shipping: {
+          address,
+          name,
+        },
+      };
+
+      console.log("order: ", order);
+      const newOrder = new Order(order);
+      await newOrder.save();
+      console.log("Order saved!");
+    }
   },
 };
 
