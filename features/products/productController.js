3f62564fd09ffdd098b1d6bc52cd55ab096b416c
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-02 20:34:25 +0100

    refactor: moved logic to separate service file.

--- features/products/productController.js
+++ features/products/productController.js
@@ -1,125 +1,36 @@
+const productService = require("../../services/productService");
 const Product = require("./productModel");
-const { buildQueryFromSearchOptions } = require("./utils/buildQueryFromSearchOptions");
 
 const getAllProducts = async (requestQuery) => {
-  let {
-    name,
-    category,
-    brand,
-    color,
-    size,
-    sort = "-amountSold",
-    page = 1,
-    pagesize: pageSize = 10,
-  } = requestQuery;
-
-  page = Number(page);
-  pageSize = Number(pageSize);
-
-  const searchOptions = {
-    name,
-    category,
-    brand,
-    color,
-    size,
-  };
-
-  let query = Product.find();
-
-  query = buildQueryFromSearchOptions(query, searchOptions);
-
-  query.sort(sort);
-
-  const queryCount = await query.clone().count();
-
-  query.skip((page - 1) * pageSize).limit(pageSize);
-
-  const queryResponse = await query.exec();
-
-  const totalPages = Math.ceil(queryCount / pageSize);
-
-  const response = {
-    products: queryResponse,
-    pagination: {
-      currentPage: page,
-      pageSize,
-      resultsCount: queryCount,
-      totalPages,
-    },
-  };
-  return response;
+  return await productService.getAll(requestQuery);
 };
 
 const getProduct = async (id) => {
-  return await Product.findById(id);
+  return await productService.get(id);
 };
 
 const getProductsByCategory = async (categoryName) => {
-  return await Product.find({
-    categories: { $in: [categoryName] },
-  });
+  return await productService.getByCategory(categoryName);
 };
 
 const addNewProduct = async (product) => {
-  const newProduct = new Product(product);
-  return await newProduct.save();
+  return await productService.createNew(product);
 };
 
 const updateProduct = async (id, updatedProduct) => {
-  return await Product.findByIdAndUpdate(id, updatedProduct, {
-    runValidators: true,
-    new: true,
-  });
+  return await productService.update(id, updatedProduct);
 };
 
-const deleteProduct = (id) => {
-  return Product.findOneAndDelete({ _id: id });
+const deleteProduct = async (id) => {
+  return await productService.delete(id);
 };
 
 const deleteManyProducts = async (ids) => {
-  return await Product.deleteMany({
-    _id: { $in: ids },
-  });
+  return await productService.deleteMany(ids);
 };
 
 const getAllFilters = async () => {
-  const allBrands = await Product.aggregate([
-    {
-      $group: {
-        _id: "$brand",
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $sort: { _id: 1 },
-    },
-  ]);
-
-  const allSizes = await Product.aggregate([
-    {
-      $group: {
-        _id: "$size",
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $sort: { _id: 1 },
-    },
-  ]);
-
-  const allColors = await Product.aggregate([
-    {
-      $group: {
-        _id: "$color",
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $sort: { _id: 1 },
-    },
-  ]);
-
-  return { allBrands, allColors, allSizes };
+  return await productService.getFilters();
 };
 
 module.exports = {
