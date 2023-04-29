8607b3a587e214fe08e843abd13cf516c7bc44b5
Kalvin Hart <kalvinhart@hotmail.co.uk>
2023-04-29 21:47:12 +0100

    removed express backend from project

--- config/userRoles.js
+++
@@ -1,4 +0,0 @@
-module.exports = ROLES = {
-  User: 1996,
-  Admin: 1987,
-};
--- features/uploads/uploadController.js
+++
@@ -1,9 +0,0 @@
-const uploadService = require("../../services/uploadService");
-
-const getUrl = async (id, name) => {
-  return await uploadService.generateUploadUrl(id, name);
-};
-
-module.exports = {
-  getUrl,
-};
